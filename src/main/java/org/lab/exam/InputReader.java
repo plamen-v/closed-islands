da5a04a1e0b2963c7252c3744ba286c550fb3bca
Plamen Vasilev <plamen.v2.1.0@gmail.com>
2024-09-29 16:26:06 +0300

    Added Input validation add README

--- src/main/java/org/lab/exam/InputReader.java
+++ src/main/java/org/lab/exam/InputReader.java
@@ -2,28 +2,35 @@ package org.lab.exam;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputReader {
-    public static int[][] read(String file){
+    public static int[][] read(String file) throws Exception {
         List<int[]> list = new ArrayList<>();
-        try {
             File myObj = new File(file);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                int[] data = Arrays.stream(myReader.nextLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
-                list.add(data);
+                String[] split = myReader.nextLine().split(" ");
+                int[] row = new int[split.length];
+                for (int i = 0; i < split.length; i++) {
+                    int val = parseInputValue(split[i]);
+                    row[i] = val;
+                }
+                list.add(row);
             }
             myReader.close();
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
 
         int[][] input = new int[list.size()][];
         input = list.toArray(input);
-        return input;
+        return list.toArray(input);
+    }
+
+    private static int parseInputValue(String s) throws Exception {
+        int i = Integer.parseInt(s);
+        if(i < 0 || i > 1 ){
+            throw new Exception("Input value out of range [0,1].");
+        }
+        return i;
     }
 }
--- src/main/java/org/lab/exam/Main.java
+++ src/main/java/org/lab/exam/Main.java
@@ -2,16 +2,21 @@ package org.lab.exam;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length > 0){
-            int[][] map = InputReader.read(args[0]);
-            Cartographer cartographer = new Cartographer();
-            System.out.println(cartographer.getClosedIslandsCount(map));
-        }else{
-            System.out.println("Provide input file with formated data.");
-            System.out.println("Example:");
-            System.out.println("1 0 0");
-            System.out.println("0 1 0");
-            System.out.println("0 0 1");
+        try {
+            if (args.length > 0) {
+                int[][] map = InputReader.read(args[0]);
+                Cartographer cartographer = new Cartographer();
+                System.out.println(cartographer.getClosedIslandsCount(map));
+            } else {
+                System.out.println("Please provide the path to the input file as parameter.");
+                System.out.println("Example:");
+                System.out.println("1 0 0");
+                System.out.println("0 1 0");
+                System.out.println("0 0 1");
+            }
+        } catch (Exception ex) {
+            System.out.println("An error occurred.");
+            ex.printStackTrace();
         }
     }
 }
